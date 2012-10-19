200bfc7491475cc16b7c81f9893ef995f84c86d6
Paul Moore <p.f.moore@gmail.com>
2012-10-19 23:24:58 +0100

    Re-enable sdist support on Windows

--- setup.py
+++ setup.py
@@ -104,9 +104,9 @@ else:
 # Under Windows, 'sdist' has not been supported.  Now that the docs build with
 # Sphinx it might work, but let's not turn it on until someone confirms that it
 # actually works.
-if os_name == 'windows' and 'sdist' in sys.argv:
-    print('The sdist command is not available under Windows.  Exiting.')
-    sys.exit(1)
+# if os_name == 'windows' and 'sdist' in sys.argv:
+#     print('The sdist command is not available under Windows.  Exiting.')
+#     sys.exit(1)
 
 #-------------------------------------------------------------------------------
 # Things related to the IPython documentation
